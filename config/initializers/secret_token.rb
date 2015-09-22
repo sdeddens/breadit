4074ce0cb372b3b2e9394085b2578e261927e762
Stephen Deddens <sdeddens@gmail.com>
2015-09-22 15:35:01 -0500

    initilize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bredit::Application.config.secret_token = '6821daeb3e09f6035904d1788c91fb2794b183646c8061dd1124c778e9df2d369441692391e9f9fd5a2b5111496d8bde1e136d44ea68f0e0270bd81092cdfa3b'
